6455960693aa5e7d39ad772fc4c971883c6f8a61
Nipun Dayanath <nipund@users.noreply.github.com>
2019-10-01 10:37:38 -0500

    Update hacktoberProfile.js

--- hacktoberProfile.js
+++ hacktoberProfile.js
@@ -59,5 +59,10 @@ var profiles = [
         "place" : "Uppsala, Sweden",
         "github" : "Frewacom"
     },
+    {
+        "name" : "Nipun Dayanath",
+        "place" : "IA, United States",
+        "github" : "nipund"
+    }
 ]
 
